5f84bd7e9ea7fd47a2ddb71df48a01f6f382b2aa
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2021-02-25 23:40:00 +0100

    Removed oolean isAdmin since the Type ENUM covers it's functionality

--- server/src/main/java/nl/tudelft/oopp/demo/entities/users/ElevatedUser.java
+++ server/src/main/java/nl/tudelft/oopp/demo/entities/users/ElevatedUser.java
@@ -1,6 +1,5 @@
 package nl.tudelft.oopp.demo.entities.users;
 
-import java.util.Objects;
 import javax.persistence.Entity;
 
 /**
@@ -8,8 +7,6 @@ import javax.persistence.Entity;
  */
 @Entity
 public class ElevatedUser extends User {
-    private boolean isAdmin;
-
     /**
      * Instantiates a new Elevated user.
      *
@@ -19,8 +16,6 @@ public class ElevatedUser extends User {
      */
     public ElevatedUser(String username, String ip, boolean isAdmin) {
         super(username, ip);
-        this.isAdmin = isAdmin;
-        setType(Type.ADMIN);
 
         if (isAdmin) {
             setType(Type.ADMIN);
@@ -37,7 +32,6 @@ public class ElevatedUser extends User {
      */
     public ElevatedUser(String username, String ip) {
         super(username, ip);
-        this.isAdmin = false;
 
         setType(Type.MODERATOR);
     }
@@ -47,50 +41,5 @@ public class ElevatedUser extends User {
      */
     public ElevatedUser() {
 
-    }
-
-    /**
-     * Is admin boolean.
-     *
-     * @return the boolean
-     */
-    public boolean isAdmin() {
-        return isAdmin;
-    }
-
-    /**
-     * Sets is admin.
-     *
-     * @param isAdmin the is admin
-     */
-    public void setIsAdmin(boolean isAdmin) {
-        this.isAdmin = isAdmin;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof ElevatedUser)) {
-            return false;
-        }
-        if (!super.equals(o)) {
-            return false;
-        }
-        ElevatedUser that = (ElevatedUser) o;
-        return isAdmin == that.isAdmin;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), isAdmin);
-    }
-
-    @Override
-    public String toString() {
-        return "ElevatedUser{"
-            + "isAdmin=" + isAdmin
-            + '}';
     }
 }
