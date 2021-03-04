e056a649b44df6f0fba040ee7747e5177138b167
AntoniosBarotsis <a.barotsis@student.tudelft.nl>
2021-03-04 11:38:16 +0100

    Added some queries

--- server/src/main/java/nl/tudelft/oopp/demo/repositories/RoomRepository.java
+++ server/src/main/java/nl/tudelft/oopp/demo/repositories/RoomRepository.java
@@ -1,9 +1,25 @@
 package nl.tudelft.oopp.demo.repositories;
 
+import java.util.List;
+import java.util.Set;
+import javax.transaction.Transactional;
 import nl.tudelft.oopp.demo.entities.Room;
+import nl.tudelft.oopp.demo.entities.questions.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
+    @Transactional
+    @Query(value = "SELECT normal_password FROM rooms WHERE id = ?1", nativeQuery = true)
+    String getPublicPassword(long roomId);
+
+    @Transactional
+    @Query(value = "SELECT elevated_password FROM rooms WHERE id = ?1", nativeQuery = true)
+    String getPrivatePassword(long roomId);
+
+    @Transactional
+    @Query(value = "SELECT q FROM Question q, Room r where r.id = ?1 AND q.id = r.id")
+    Set<Question> findAllQuestions(long roomId);
 }
