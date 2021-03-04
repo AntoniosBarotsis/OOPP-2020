74b5ef596ef84e79135f01cebce209505a2a7aea
AntoniosBarotsis <a.barotsis@student.tudelft.nl>
2021-03-04 12:45:02 +0100

    Added questions, passwords and pace endpoints
    Poll left, waiting for MR

--- server/src/main/java/nl/tudelft/oopp/demo/controllers/RoomController.java
+++ server/src/main/java/nl/tudelft/oopp/demo/controllers/RoomController.java
@@ -8,41 +8,119 @@ import nl.tudelft.oopp.demo.services.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * The type Room controller.
+ */
 @RestController("Room")
 @RequestMapping("api/v1/rooms")
 public class RoomController {
     @Autowired
     private final RoomService roomService;
 
+    /**
+     * Instantiates a new Room controller.
+     *
+     * @param roomService the room service
+     */
     public RoomController(RoomService roomService) {
         this.roomService = roomService;
     }
 
+    /**
+     * Find all list.
+     *
+     * @return the list
+     */
     @GetMapping
     public List<Room> findAll() {
         return roomService.findAll();
     }
 
+    /**
+     * Gets one.
+     *
+     * @param id the id
+     * @return the one
+     */
     @GetMapping("{id}")
     public Room getOne(@PathVariable long id) {
         return roomService.getOne(id);
     }
 
+    /**
+     * Gets public password.
+     *
+     * @param roomId the room id
+     * @return the public password
+     */
     @GetMapping("public/{roomId}")
     public String getPublicPassword(@PathVariable long roomId) {
         return roomService.getPublicPassword(roomId);
     }
 
+    /**
+     * Gets private password.
+     *
+     * @param roomId the room id
+     * @return the private password
+     */
     @GetMapping("private/{roomId}")
     public String getPrivatePassword(@PathVariable long roomId) {
         return roomService.getPrivatePassword(roomId);
     }
 
+    /**
+     * Find all questions set.
+     *
+     * @param roomId the room id
+     * @return the set
+     */
     @GetMapping("questions/{roomId}")
     public Set<Question> findAllQuestions(@PathVariable long  roomId) {
         return roomService.findAllQuestions(roomId);
+    }
+
+    /**
+     * Increment too fast.
+     *
+     * @param roomId the room id
+     */
+    @PutMapping("/increment/tooFast/{roomId}")
+    public void incrementTooFast(@PathVariable long roomId) {
+        roomService.incrementTooFast(roomId);
+    }
+
+    /**
+     * Increment too fast.
+     *
+     * @param roomId the room id
+     */
+    @PutMapping("/decrement/tooFast/{roomId}")
+    public void decrementTooFast(@PathVariable long roomId) {
+        roomService.decrementTooFast(roomId);
+    }
+
+    /**
+     * Increment too fast.
+     *
+     * @param roomId the room id
+     */
+    @PutMapping("/increment/tooSlow/{roomId}")
+    public void incrementTooSlow(@PathVariable long roomId) {
+        roomService.incrementTooSlow(roomId);
+    }
+
+    /**
+     * Increment too fast.
+     *
+     * @param roomId the room id
+     */
+    @PutMapping("/decrement/tooSlow/{roomId}")
+    public void decrementTooSlow(@PathVariable long roomId) {
+        roomService.decrementTooSlow(roomId);
     }
 }
--- server/src/main/java/nl/tudelft/oopp/demo/services/RoomService.java
+++ server/src/main/java/nl/tudelft/oopp/demo/services/RoomService.java
@@ -8,33 +8,106 @@ import nl.tudelft.oopp.demo.repositories.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * The type Room service.
+ */
 @Service
 public class RoomService {
     @Autowired
     private final RoomRepository roomRepository;
 
+    /**
+     * Instantiates a new Room service.
+     *
+     * @param roomRepository the room repository
+     */
     public RoomService(RoomRepository roomRepository) {
         this.roomRepository = roomRepository;
     }
 
+    /**
+     * Find all list.
+     *
+     * @return the list
+     */
     public List<Room> findAll() {
         return roomRepository.findAll();
     }
 
+    /**
+     * Gets one.
+     *
+     * @param id the id
+     * @return the one
+     */
     public Room getOne(long id) {
         return roomRepository.getOne(id);
     }
 
+    /**
+     * Gets public password.
+     *
+     * @param roomId the room id
+     * @return the public password
+     */
     public String getPublicPassword(long roomId) {
         return roomRepository.getPublicPassword(roomId);
     }
 
+    /**
+     * Gets private password.
+     *
+     * @param roomId the room id
+     * @return the private password
+     */
     public String getPrivatePassword(long roomId) {
         // Later perform some sort of check here to see if the user has permission
         return roomRepository.getPrivatePassword(roomId);
     }
 
+    /**
+     * Find all questions set.
+     *
+     * @param roomId the room id
+     * @return the set
+     */
     public Set<Question> findAllQuestions(long roomId) {
         return roomRepository.findAllQuestions(roomId);
+    }
+
+    /**
+     * Increment too fast.
+     *
+     * @param roomId the room id
+     */
+    public void incrementTooFast(long roomId) {
+        roomRepository.incrementTooFast(roomId);
+    }
+
+    /**
+     * Decrement too fast.
+     *
+     * @param roomId the room id
+     */
+    public void decrementTooFast(long roomId) {
+        roomRepository.decrementTooFast(roomId);
+    }
+
+    /**
+     * Increment too slow.
+     *
+     * @param roomId the room id
+     */
+    public void incrementTooSlow(long roomId) {
+        roomRepository.incrementTooSlow(roomId);
+    }
+
+    /**
+     * Decrement too slow.
+     *
+     * @param roomId the room id
+     */
+    public void decrementTooSlow(long roomId) {
+        roomRepository.decrementTooSlow(roomId);
     }
 }
