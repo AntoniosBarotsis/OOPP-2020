b2edd043ef9e1747c866d07e7040f2d528a47134
AntoniosBarotsis <a.barotsis@student.tudelft.nl>
2021-03-12 17:59:27 +0100

    Made the question class use Lombok annotations

--- server/src/main/java/nl/tudelft/oopp/demo/entities/Question.java
+++ server/src/main/java/nl/tudelft/oopp/demo/entities/Question.java
@@ -16,6 +16,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import nl.tudelft.oopp.demo.entities.users.User;
 
 /**
@@ -23,6 +25,8 @@ import nl.tudelft.oopp.demo.entities.users.User;
  */
 @Entity(name = "Question")
 @Table(name = "questions")
+@Data
+@NoArgsConstructor
 public class Question {
     @Id
     @SequenceGenerator(
@@ -74,13 +78,6 @@ public class Question {
     }
 
     /**
-     * Instantiates a new Question.
-     */
-    public Question() {
-
-    }
-
-    /**
      * Export question to txt string.
      *
      * @return the string
@@ -99,207 +96,6 @@ public class Question {
         ObjectMapper objMapper = new ObjectMapper();
 
         return objMapper.writeValueAsString(this);
-    }
-
-    /**
-     * Gets id.
-     *
-     * @return the id
-     */
-    public long getId() {
-        return id;
-    }
-
-    /**
-     * Sets id.
-     *
-     * @param id the id
-     */
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    /**
-     * Gets title.
-     *
-     * @return the title
-     */
-    public String getTitle() {
-        return title;
-    }
-
-    /**
-     * Sets title.
-     *
-     * @param title the title
-     */
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    /**
-     * Gets text.
-     *
-     * @return the text
-     */
-    public String getText() {
-        return text;
-    }
-
-    /**
-     * Sets text.
-     *
-     * @param text the text
-     */
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    /**
-     * Gets author.
-     *
-     * @return the author
-     */
-    public User getAuthor() {
-        return author;
-    }
-
-    /**
-     * Sets author.
-     *
-     * @param author the author
-     */
-    public void setAuthor(User author) {
-        this.author = author;
-    }
-
-    /**
-     * Gets upvotes.
-     *
-     * @return the upvotes
-     */
-    public int getUpvotes() {
-        return upvotes;
-    }
-
-    /**
-     * Sets upvotes.
-     *
-     * @param upvotes the upvotes
-     */
-    public void setUpvotes(int upvotes) {
-        this.upvotes = upvotes;
-    }
-
-    /**
-     * Gets score.
-     *
-     * @return the score
-     */
-    public int getScore() {
-        return score;
-    }
-
-    /**
-     * Sets score.
-     *
-     * @param score the score
-     */
-    public void setScore(int score) {
-        this.score = score;
-    }
-
-    /**
-     * Gets time created.
-     *
-     * @return the time created
-     */
-    public Date getTimeCreated() {
-        return timeCreated;
-    }
-
-    /**
-     * Sets time created.
-     *
-     * @param timeCreated the time created
-     */
-    public void setTimeCreated(Date timeCreated) {
-        this.timeCreated = timeCreated;
-    }
-
-    /**
-     * Gets status.
-     *
-     * @return the status
-     */
-    public QuestionStatus getStatus() {
-        return status;
-    }
-
-    /**
-     * Sets status.
-     *
-     * @param status the status
-     */
-    public void setStatus(QuestionStatus status) {
-        this.status = status;
-    }
-
-    /**
-     * Gets answer.
-     *
-     * @return the answer
-     */
-    public String getAnswer() {
-        return answer;
-    }
-
-    /**
-     * Sets answer.
-     *
-     * @param answer the answer
-     */
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Question)) {
-            return false;
-        }
-        Question question = (Question) o;
-        return id == question.id && upvotes == question.upvotes && score == question.score
-            && Objects.equals(title, question.title)
-            && Objects.equals(text, question.text)
-            && Objects.equals(author, question.author)
-            && Objects.equals(timeCreated, question.timeCreated)
-            && status == question.status
-            && Objects.equals(answer, question.answer);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects
-            .hash(id, title, text, author, upvotes, score, timeCreated, status, answer);
-    }
-
-    @Override
-    public String toString() {
-        return "Question{"
-            + ", id=" + id
-            + ", title='" + title + '\''
-            + ", text='" + text + '\''
-            + ", author=" + author
-            + ", upvotes=" + upvotes
-            + ", score=" + score
-            + ", timeCreated=" + timeCreated
-            + ", status=" + status
-            + ", answer='" + answer + '\''
-            + '}';
     }
 
     /**
