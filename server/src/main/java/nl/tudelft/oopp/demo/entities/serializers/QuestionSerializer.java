7d4b7045eba31bd0ff4caaf0c929aa4eb14df827
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2021-03-13 22:25:45 +0100

    Added question serializer

---
+++ server/src/main/java/nl/tudelft/oopp/demo/entities/serializers/QuestionSerializer.java
@@ -0,0 +1,46 @@
+package nl.tudelft.oopp.demo.entities.serializers;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import nl.tudelft.oopp.demo.entities.Question;
+
+
+/**
+ * The type Question serializer. This limits the Question files to
+ * text, answer and timeCreated
+ */
+public class QuestionSerializer extends StdSerializer<Question> {
+
+    /**
+     * Instantiates a new Question serializer.
+     */
+    public QuestionSerializer() {
+        this(null);
+    }
+
+    /**
+     * Instantiates a new Question serializer.
+     *
+     * @param t the t
+     */
+    protected QuestionSerializer(Class<Question> t) {
+        super(t);
+    }
+
+    @Override
+    public void serialize(Question value, JsonGenerator gen, SerializerProvider provider)
+        throws IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+        String strDate = dateFormat.format(value.getTimeCreated());
+
+        gen.writeStartObject();
+        gen.writeStringField("text", value.getText());
+        gen.writeStringField("answer", value.getAnswer());
+        gen.writeStringField("timeCreated", strDate);
+        gen.writeEndObject();
+    }
+}
