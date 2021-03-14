46bde4be663c2c2db2e661ee442772e141ff84d4
Kiril Vasilev <k.v.vasilev-1@student.tudelft.nl>
2021-03-14 10:27:44 +0100

    Add Main Menu Lecturer question export options

--- client/src/main/java/nl/tudelft/oopp/demo/communication/mainmenu/MainModCommunication.java
+++ client/src/main/java/nl/tudelft/oopp/demo/communication/mainmenu/MainModCommunication.java
@@ -21,4 +21,34 @@ public class MainModCommunication extends MainMenuCommunication {
         return  requestStringData(link + id);
     }
 
+    /**
+     * Request all questions for a chosen room.
+     * @param id id of a room
+     * @return all questions in json format
+     */
+    public static String getAllQuestions(long id) {
+        String link = "http://localhost:8080/api/v1/questions/exportAll?roomId=";
+        return  requestStringData(link + id);
+    }
+
+    /**
+     * Request top 20 questions for a chosen room.
+     * @param id id of a room
+     * @return top 20 questions in json format
+     */
+    public static String getTopQuestions(long id) {
+        String link = "http://localhost:8080/api/v1/questions/exportTop?amount=20&roomId=";
+        return  requestStringData(link + id);
+    }
+
+    /**
+     * Request all questions with text answer for a chosen room.
+     * @param id id of a room
+     * @return answered questions in json format
+     */
+    public static String getAnsweredQuestions(long id) {
+        String link = "http://localhost:8080/api/v1/questions/exportAnswered?roomId=";
+        return  requestStringData(link + id);
+    }
+
 }
