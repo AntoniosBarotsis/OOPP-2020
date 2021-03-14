a0efb04441896f0725a00ca5ba57d02944250791
AntoniosBarotsis <a.barotsis@student.tudelft.nl>
2021-03-14 13:13:48 +0100

    Added ddQuestionToUser method

--- server/src/main/java/nl/tudelft/oopp/demo/repositories/UserRepository.java
+++ server/src/main/java/nl/tudelft/oopp/demo/repositories/UserRepository.java
@@ -1,9 +1,25 @@
 package nl.tudelft.oopp.demo.repositories;
 
+import javax.transaction.Transactional;
 import nl.tudelft.oopp.demo.entities.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    /**
+     * Add question to user.
+     *
+     * @param userId           the user id
+     * @param questionsAskedId the questions asked id
+     */
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO user_questions_asked (user_id, questions_asked_id) VALUES (?1, ?2)",
+        nativeQuery = true)
+    void addQuestionToUser(long userId, long questionsAskedId);
 }
