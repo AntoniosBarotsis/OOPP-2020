c2f034e1e7a78660d3bd28921ebff65dbc619708
Daniel Puente Barajas <D.PuenteBarajas@student.tudelft.nl>
2021-03-16 00:52:11 +0100

    Created the Javadoc

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/questions/OwnQuestionController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/questions/OwnQuestionController.java
@@ -34,6 +34,12 @@ public class OwnQuestionController {
     @FXML
     private TextArea score;
 
+    /**
+     * Takes the relevant information from the question, user and app and creates a JavaFX object with all the relevant information
+     * @param question the question asked by the user
+     * @param user the user information in case of a need to ban
+     * @param room the room information
+     */
     public void loadData(Question question, User user, Room room) {
         this.room = room;
         this.user = user;
@@ -43,29 +49,39 @@ public class OwnQuestionController {
         score.setText(Integer.toString(question.getScore()));
     }
 
+    /**
+     * Takes a question and increases/decreases the votes, depending on the user interaction with the upvoting button.
+     * Turns the button grey when upvoted and blue when decreased.
+     */
     @FXML
     private void upvote() {
 
         if(upvoted){
-            //QuestionViewCommunication.upvote(question.getId());
+            QuestionViewCommunication.upvote(question.getId());
             score.setText(String.valueOf(Integer.parseInt(score.getText()) - 1));
             upvoteButton.setStyle("-fx-text-fill: #00A6D6");
             upvoted = false;
         }
         else{
-            //QuestionViewCommunication.downvote(question.getId());
+            QuestionViewCommunication.downvote(question.getId());
             score.setText(String.valueOf(Integer.parseInt(score.getText()) + 1));
             upvoteButton.setStyle("-fx-text-fill: #808080");
             upvoted = true;
         }
     }
 
+    /**
+     * Deletes the marked question
+     */
     public void deleteQuestion() {
 
     }
 
+    /**
+     * Marks the question as answered, hiding it from this
+     */
     public void questionAnswered() {
-        //QuestionViewCommunication.userMarkAsAnswer(question.getId());
+        QuestionViewCommunication.userMarkAsAnswer(question.getId());
 
     }
 }
