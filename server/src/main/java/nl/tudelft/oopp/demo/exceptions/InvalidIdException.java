b92c0c96f512e9d30541500dc89f5184213805e2
AntoniosBarotsis <a.barotsis@student.tudelft.nl>
2021-03-17 13:58:29 +0100

    Switched to RunTime exceptions

--- server/src/main/java/nl/tudelft/oopp/demo/exceptions/InvalidIdException.java
+++ server/src/main/java/nl/tudelft/oopp/demo/exceptions/InvalidIdException.java
@@ -1,6 +1,6 @@
 package nl.tudelft.oopp.demo.exceptions;
 
-public class InvalidIdException extends Exception {
+public class InvalidIdException extends RuntimeException {
     public InvalidIdException(String message) {
         super(message);
     }
--- server/src/main/java/nl/tudelft/oopp/demo/exceptions/InvalidPasswordException.java
+++ server/src/main/java/nl/tudelft/oopp/demo/exceptions/InvalidPasswordException.java
@@ -1,6 +1,6 @@
 package nl.tudelft.oopp.demo.exceptions;
 
-public class InvalidPasswordException extends Exception {
+public class InvalidPasswordException extends RuntimeException {
     public InvalidPasswordException(String message) {
         super(message);
     }
