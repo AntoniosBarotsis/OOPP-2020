791792f99eec05a438ec554870edf1624baaa24d
AntoniosBarotsis <a.barotsis@student.tudelft.nl>
2021-03-17 19:12:27 +0100

    Added v1 methods

--- server/src/main/java/nl/tudelft/oopp/demo/services/QuestionService.java
+++ server/src/main/java/nl/tudelft/oopp/demo/services/QuestionService.java
@@ -5,12 +5,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import nl.tudelft.oopp.demo.entities.Question;
 import nl.tudelft.oopp.demo.entities.serializers.QuestionExportSerializer;
 import nl.tudelft.oopp.demo.entities.serializers.QuestionSerializer;
+import nl.tudelft.oopp.demo.entities.users.User;
 import nl.tudelft.oopp.demo.exceptions.InvalidIdException;
 import nl.tudelft.oopp.demo.exceptions.UnauthorizedException;
 import nl.tudelft.oopp.demo.repositories.QuestionRepository;
@@ -51,6 +53,203 @@ public class QuestionService {
         questionRepository.addQuestion(roomId, question.getId());
 
         userRepository.addQuestionToUser(question.getAuthor().getId(), question.getId());
+    }
+
+    /**
+     * Gets the text of the question.
+     *
+     * @param questionId the question id
+     * @return a String of the text of the question
+     */
+    public String getQuestion(long questionId) {
+        return questionRepository.getQuestion(questionId);
+    }
+
+    /**
+     * Sets the text of the question to be newQuestion.
+     *
+     * @param questionId the question id
+     * @param newQuestion the value of text that will be set as question's text
+     */
+    public void editQuestion(long questionId, String newQuestion) {
+        questionRepository.editQuestion(questionId, newQuestion);
+    }
+
+    /**
+     * Gets the author.
+     *
+     * @param questionId the question id
+     * @return the author of the question
+     */
+    public User getAuthor(long questionId) {
+        return questionRepository.getAuthor(questionId);
+    }
+
+    /**
+     * Increases the value of upvote by 1.
+     *
+     * @param questionId the question id
+     */
+    public void upvote(long questionId) {
+        questionRepository.upvote(questionId);
+    }
+
+
+    /**
+     * Decreases the value of upvote by 1.
+     *
+     * @param questionId the question id
+     */
+    public void downvote(long questionId) {
+        if (questionRepository.getUpvotes(questionId) > 0) {
+            questionRepository.downvote(questionId);
+        }
+    }
+
+
+    /**
+     * Gets the number of upvotes.
+     *
+     * @param questionId the question id
+     * @return the value of upvotes
+     */
+    public int getUpvotes(long questionId) {
+        return questionRepository.getUpvotes(questionId);
+    }
+
+
+    /**
+     * Gets the score.
+     *
+     * @param questionId the question id
+     * @return the score value of question
+     */
+    public int getScore(long questionId) {
+        return questionRepository.getScore(questionId);
+    }
+
+
+    /**
+     * Sets the score of question with value score.
+     *
+     * @param questionId the question id
+     * @param score the new score value of question
+     */
+    public void setScore(long questionId, int score) {
+        if (score >= 0) {
+            questionRepository.setScore(questionId, score);
+        } else {
+            questionRepository.setScore(questionId, 0);
+        }
+    }
+
+
+    /**
+     * Gets the id of Question with the number highest score. So if 0 is passed
+     * the question id of the highest score question is returned.
+     *
+     * @param number the number of question this should return
+     * @return question id of highest score Question
+     */
+    public long get(int number) {
+        return questionRepository.getHighestScore().get(number);
+    }
+
+    /**
+     * Gets the date.
+     *
+     * @param questionId the question id
+     * @return the question date
+     */
+    public Date getTime(long questionId) {
+        return questionRepository.getTime(questionId);
+    }
+
+
+    /**
+     * Gets the status of question.
+     *
+     * @param questionId the question id
+     * @return the status of question
+     */
+    public Question.QuestionStatus getStatus(long questionId) {
+        return questionRepository.getStatus(questionId);
+    }
+
+
+    /**
+     *Sets the value of status as ANSWERED.
+     *
+     * @param questionId the question id
+     */
+    public void setAnswered(long questionId) {
+        questionRepository.setAnswered(questionId);
+    }
+
+    /**
+     *Sets the value of status as ANSWERED, unless its score is greater than 5.
+     *
+     * @param questionId the question id
+     */
+    public void userSetAnswered(long questionId) {
+        if (questionRepository.getScore(questionId) <= 5) {
+            questionRepository.setAnswered(questionId);
+        }
+    }
+
+    /**
+     *Sets the value of status as ANSWERED, unless its score is greater than maxScore.
+     *
+     * @param maxScore the max score for changing the status to answered
+     * @param questionId the question id
+     */
+    public void userSetAnswered(long questionId, int maxScore) {
+        if (questionRepository.getScore(questionId) <= maxScore) {
+            questionRepository.setAnswered(questionId);
+        }
+    }
+
+
+    /**
+     * Sets the value of status as SPAM.
+     *
+     * @param questionId the question id
+     */
+    public void setSpam(long questionId) {
+        questionRepository.setSpam(questionId);
+    }
+
+
+
+    /**
+     * Sets the value of status as OPEN.
+     *
+     * @param questionId the question id
+     */
+    public void setOpen(long questionId) {
+        questionRepository.setOpen(questionId);
+    }
+
+
+    /**
+     * Gets the answer of the question.
+     *
+     * @param questionId the question id
+     * @return the answer
+     */
+    public String getAnswer(long questionId) {
+        return  questionRepository.getAnswer(questionId);
+    }
+
+
+    /**
+     * Sets the answer of question as answer.
+     *
+     * @param questionId the question id
+     * @param answer the new answer of question
+     */
+    public void setAnswer(long questionId, String answer) {
+        questionRepository.setAnswer(questionId, answer);
     }
 
     /**
