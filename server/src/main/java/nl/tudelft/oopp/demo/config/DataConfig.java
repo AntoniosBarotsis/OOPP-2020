69dbf6c2d8b546c7984fb0246af4143c902c0247
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2021-03-19 22:58:47 +0100

    Cleanup

--- server/src/main/java/nl/tudelft/oopp/demo/config/DataConfig.java
+++ server/src/main/java/nl/tudelft/oopp/demo/config/DataConfig.java
@@ -26,29 +26,9 @@ import nl.tudelft.oopp.demo.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class DataConfig {
-
-    /**
-     * Api docket for swagger.
-     *
-     * @return the docket
-     */
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-            .select()
-            .apis(RequestHandlerSelectors.any())
-            .paths(PathSelectors.any())
-            .build();
-    }
 
     /**
      * Command line runner command line runner.
