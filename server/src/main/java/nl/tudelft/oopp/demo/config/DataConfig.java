c51504ba71e737e8a0478a0bdd66ef6936dd9a9a
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2021-03-19 22:34:55 +0100

    Added swagger

--- server/src/main/java/nl/tudelft/oopp/demo/config/DataConfig.java
+++ server/src/main/java/nl/tudelft/oopp/demo/config/DataConfig.java
@@ -26,10 +26,40 @@ import nl.tudelft.oopp.demo.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class DataConfig {
 
+    /**
+     * Api docket for swagger.
+     *
+     * @return the docket
+     */
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build();
+    }
+
+    /**
+     * Command line runner command line runner.
+     *
+     * @param quoteRepository    the quote repository
+     * @param userRepository     the user repository
+     * @param roomRepository     the room repository
+     * @param questionRepository the question repository
+     * @param pollRepository     the poll repository
+     * @return the command line runner
+     */
     @Bean
     CommandLineRunner commandLineRunner(QuoteRepository quoteRepository,
                                         UserRepository userRepository,
