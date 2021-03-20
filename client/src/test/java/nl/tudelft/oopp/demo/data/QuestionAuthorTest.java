321a3068aedbadce6217ab59d79b4af7dce56260
Kiril Vasilev <k.v.vasilev-1@student.tudelft.nl>
2021-03-20 11:55:35 +0100

    Fix failing builds

--- client/src/test/java/nl/tudelft/oopp/demo/data/QuestionAuthorTest.java
+++ client/src/test/java/nl/tudelft/oopp/demo/data/QuestionAuthorTest.java
@@ -1,11 +1,13 @@
 package nl.tudelft.oopp.demo.data;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 class QuestionAuthorTest {
 
     private QuestionAuthor author = new QuestionAuthor(42, "universe");
+
     @Test
     void getId() {
         assertEquals(42, author.getId());
