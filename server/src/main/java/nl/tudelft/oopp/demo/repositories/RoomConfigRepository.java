f9315178a56801df7d84776633cc206020036ace
AntoniosBarotsis <a.barotsis@student.tudelft.nl>
2021-03-22 22:55:50 +0100

    Config repo

---
+++ server/src/main/java/nl/tudelft/oopp/demo/repositories/RoomConfigRepository.java
@@ -0,0 +1,35 @@
+package nl.tudelft.oopp.demo.repositories;
+
+import javax.transaction.Transactional;
+import nl.tudelft.oopp.demo.entities.RoomConfig;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+/**
+ * The interface Room config repository.
+ */
+public interface RoomConfigRepository extends JpaRepository<RoomConfig, Long> {
+
+    /**
+     * Sets student refresh rate.
+     *
+     * @param roomId             the room id
+     * @param studentRefreshRate the student refresh rate
+     * @param modRefreshRate     the mod refresh rate
+     * @param questionCooldown   the question cooldown
+     * @param paceCooldown       the pace cooldown
+     */
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE ROOM_CONFIG c "
+        + "SET c.STUDENT_REFRESH_RATE = ?2, "
+        + "c.MOD_REFRESH_RATE = ?3, "
+        + "c.QUESTION_COOLDOWN = ?4, "
+        + "c.PACE_COOLDOWN = ?5 "
+        + "WHERE EXISTS "
+        + "(SELECT * FROM ROOMS r WHERE r.ROOM_CONFIG_ID = c.id AND r.id = ?1)",
+        nativeQuery = true)
+    void setConfig(long roomId, int studentRefreshRate, int modRefreshRate, int questionCooldown,
+                   int paceCooldown);
+}
