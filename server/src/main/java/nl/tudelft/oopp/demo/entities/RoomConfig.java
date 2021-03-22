fd4840ba05eb1d8a6c48200e4bd752d5ea6f7ab5
AntoniosBarotsis <a.barotsis@student.tudelft.nl>
2021-03-22 22:52:42 +0100

    Checkstyle

--- server/src/main/java/nl/tudelft/oopp/demo/entities/RoomConfig.java
+++ server/src/main/java/nl/tudelft/oopp/demo/entities/RoomConfig.java
@@ -2,17 +2,13 @@ package nl.tudelft.oopp.demo.entities;
 
 import static javax.persistence.GenerationType.SEQUENCE;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 /**
  * The type Room config.
@@ -33,9 +29,6 @@ public class RoomConfig {
         generator = "room_config_sequence"
     )
     private int id;
-//    @OneToOne(cascade = CascadeType.MERGE, optional = false)
-//    @JoinColumn(name = "room_id")
-//    private Room room;
     @Column(name = "student_refresh_rate")
     private int studentRefreshRate;
     @Column(name = "mod_refresh_rate")
@@ -45,14 +38,11 @@ public class RoomConfig {
     @Column(name = "pace_cooldown")
     private int paceCooldown;
 
+
     /**
      * Instantiates a new Room config.
-     *
-     * @param room the room
      */
     public RoomConfig() {
-//        this.room = room;
-
         this.studentRefreshRate = 5;
         this.modRefreshRate = 5;
         this.questionCooldown = 300;
