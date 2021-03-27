311a5528b44a6c7f78ed8a3f3148116b1aaeee7a
AntoniosBarotsis <a.barotsis@student.tudelft.nl>
2021-03-27 21:00:38 +0100

    Improved Documentation

--- server/src/main/java/nl/tudelft/oopp/demo/repositories/PollRepository.java
+++ server/src/main/java/nl/tudelft/oopp/demo/repositories/PollRepository.java
@@ -4,6 +4,9 @@ import nl.tudelft.oopp.demo.entities.Poll;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * The interface Poll repository.
+ */
 @Repository
 public interface PollRepository extends JpaRepository<Poll, Long> {
 }
