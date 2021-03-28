ef19141c66c5c24c61c3ad5128644f68412e994a
Kiril Vasilev <k.v.vasilev-1@student.tudelft.nl>
2021-03-28 20:27:29 +0200

    Test poll view controllers

---
+++ client/src/test/java/nl/tudelft/oopp/demo/controllers/polls/ModPollControllerTest.java
@@ -0,0 +1,25 @@
+package nl.tudelft.oopp.demo.controllers.polls;
+
+import org.junit.jupiter.api.Test;
+
+class ModPollControllerTest {
+
+    /**
+     * Follow the manual test plan for fetching polls from server first.
+     */
+    @Test
+    void loadData() {
+        /* Manual test plan to be carried out when testing if everything works correctly.
+        Test plan for testing if loading a poll works:
+        1. Upon launching the status should be visible on the left.
+        2. The number of participants in the middle.
+        3. The date the poll was created on the right.
+        4. The text of the poll should be visible in the centre of the view.
+         */
+    }
+
+    @Test
+    void buttonOptionsClicked() {
+        // TODO: implement the button click event
+    }
+}
---
+++ client/src/test/java/nl/tudelft/oopp/demo/controllers/polls/PollControllerTest.java
@@ -0,0 +1,17 @@
+package nl.tudelft.oopp.demo.controllers.polls;
+
+import org.junit.jupiter.api.Test;
+
+class PollControllerTest {
+
+    @Test
+    void loadData() {
+        /* Manual test plan to be carried out when testing if everything works correctly.
+        Test plan for testing if loading a poll works:
+        1. Upon launching the status should be visible on the left.
+        2. The number of participants in the middle.
+        3. The date the poll was created on the right.
+        4. The text of the poll should be visible in the centre of the view.
+         */
+    }
+}
