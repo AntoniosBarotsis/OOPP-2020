1a20b726ce147a1ab209ad43034c2f10b0540955
Roy Katz <R.Katz-1@student.tudelft.nl>
2021-03-28 19:14:20 +0200

    Fixed checkstyle violations 2

--- client/src/test/java/nl/tudelft/oopp/demo/communication/questionView/QuestionViewCommunicationTest.java
+++ client/src/test/java/nl/tudelft/oopp/demo/communication/questionView/QuestionViewCommunicationTest.java
@@ -1,11 +1,14 @@
 package nl.tudelft.oopp.demo.communication.questionView;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+
 import nl.tudelft.oopp.demo.communication.questionview.QuestionViewCommunication;
 import nl.tudelft.oopp.demo.data.helper.QuestionHelper;
 import nl.tudelft.oopp.demo.data.helper.StudentHelper;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 
 class QuestionViewCommunicationTest {
     private final String url = "http://localhost:8080/api/v2/questions/";
@@ -18,60 +21,60 @@ class QuestionViewCommunicationTest {
 
     @Test
     void upvote() {
-        assertDoesNotThrow(()->QuestionViewCommunication.upvote(1));
+        assertDoesNotThrow(() -> QuestionViewCommunication.upvote(1));
     }
 
     @Test
     void studentMarkAsAnswer() {
-        assertDoesNotThrow(()->QuestionViewCommunication.studentMarkAsAnswer(1));
+        assertDoesNotThrow(() -> QuestionViewCommunication.studentMarkAsAnswer(1));
     }
 
     @Test
     void modMarkAsAnswer() {
-        assertDoesNotThrow(()->QuestionViewCommunication.modMarkAsAnswer(1));
+        assertDoesNotThrow(() -> QuestionViewCommunication.modMarkAsAnswer(1));
     }
 
     @Test
     void banUser() {
-        assertDoesNotThrow(()->QuestionViewCommunication.banUser(1));
+        assertDoesNotThrow(() -> QuestionViewCommunication.banUser(1));
     }
 
     @Test
     void downvote() {
-        assertDoesNotThrow(()->QuestionViewCommunication.downvote(1));
+        assertDoesNotThrow(() -> QuestionViewCommunication.downvote(1));
     }
 
     @Test
     void setText() {
         StudentHelper user = new StudentHelper("Student 1", "IP address");
         QuestionHelper qhelp = new QuestionHelper("This is a question", user);
-        assertDoesNotThrow(()->QuestionViewCommunication.setText(1, qhelp));
+        assertDoesNotThrow(() -> QuestionViewCommunication.setText(1, qhelp));
     }
 
     @Test
     void setAnswer() {
         StudentHelper user = new StudentHelper("Student 1", "IP address");
         QuestionHelper qhelp = new QuestionHelper("This is an answer", user);
-        assertDoesNotThrow(()->QuestionViewCommunication.setAnswer(1, qhelp));
+        assertDoesNotThrow(() -> QuestionViewCommunication.setAnswer(1, qhelp));
     }
 
     @Test
     void addQuestionUpvoted() {
-        assertDoesNotThrow(()->QuestionViewCommunication.addQuestionUpvoted(1, 1));
+        assertDoesNotThrow(() -> QuestionViewCommunication.addQuestionUpvoted(1, 1));
     }
 
     @Test
     void removeQuestionUpvoted() {
-        assertDoesNotThrow(()->QuestionViewCommunication.removeQuestionUpvoted(1, 1));
+        assertDoesNotThrow(() -> QuestionViewCommunication.removeQuestionUpvoted(1, 1));
     }
 
     @Test
     void ban() {
-        // assertDoesNotThrow(()->QuestionViewCommunication.ban(1, 3));
+        // assertDoesNotThrow(() -> QuestionViewCommunication.ban(1, 3));
     }
 
     @Test
     void delete() {
-        // assertDoesNotThrow(()->QuestionViewCommunication.delete(1, 1));
+        // assertDoesNotThrow(() -> QuestionViewCommunication.delete(1, 1));
     }
 }
