bbc5a9111fd93fa1baddbbd4337fe370e200eeef
lesleysmits <l.smits-2@student.tudelft.nl>
2021-03-30 12:22:56 +0200

    CheckStyle fix and fxml redesign + controller fixes (MainCommunication and SettingsCommunication tests fail)

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/polls/AnswerPollController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/polls/AnswerPollController.java
@@ -1,13 +1,16 @@
 package nl.tudelft.oopp.demo.controllers.polls;
 
-import javafx.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.fxml.FXML;
+
 import javafx.scene.Cursor;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 
-import java.util.List;
+
 
 public class AnswerPollController {
 
@@ -19,45 +22,52 @@ public class AnswerPollController {
     private String background = "-fx-background-color: LightBlue";
     private String highlightedBackground = "-fx-background-color: Green";
     private int correct;
-    private Button selected;
+    private List<Button> selected = new ArrayList<>();
 
-    public void loadData(List<String> questions, int index){
-        correct = index;
+    /**
+     * Load data into the poll.
+     * @param questions List containing all the questions
+     */
+    public void loadData(List<String> questions) {
         if (questions.size() <= 2) {
             fillLists(questions, 342);
-        }
-        else if (questions.size() <= 4) {
+        } else if (questions.size() <= 4) {
             fillLists(questions, 168);
-        }
-        else if (questions.size() <=6) {
+        } else if (questions.size() <= 6) {
             fillLists(questions, 110);
-        }
-        else if (questions.size() <= 8) {
+        } else if (questions.size() <= 8) {
             fillLists(questions, 81);
-        }
-        else if (questions.size() <= 10){
+        } else if (questions.size() <= 10) {
             fillLists(questions, 63);
-        }
-        else {
+        } else {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setHeaderText(null);
             alert.setContentText("Error inserting questions!");
             alert.showAndWait();
         }
-
     }
 
+    /**
+     * Fills the listviews with buttons.
+     * @param questions List containing the questions
+     * @param height The height that a button can be
+     */
     public void fillLists(List<String> questions, int height) {
         for (int i = 0; i < questions.size(); i++) {
             if (i % 2 == 0) {
                 listLeft.getItems().add(buttonCreator(questions.get(i), height));
-            }
-            else {
+            } else {
                 listRight.getItems().add(buttonCreator(questions.get(i), height));
             }
         }
     }
 
+    /**
+     * Creates a button based on height.
+     * @param value the questions that the button represents
+     * @param height the height that the button can be
+     * @return
+     */
     public Button buttonCreator(String value, int height) {
         Button button = new Button(value);
         button.setPrefHeight(height);
@@ -65,11 +75,14 @@ public class AnswerPollController {
         button.setStyle(background);
         button.setCursor(Cursor.HAND);
         button.setOnAction(e -> {
-            if(selected != null){
-                selected.setStyle(background);
+            Button button1 = (Button) e.getSource();
+            if (selected.contains(button1)) {
+                button1.setStyle(background);
+                selected.remove(button1);
+            } else {
+                selected.add((button1));
+                button1.setStyle(highlightedBackground);
             }
-            selected = (Button) e.getSource();
-            selected.setStyle(highlightedBackground);
         });
         return button;
     }
