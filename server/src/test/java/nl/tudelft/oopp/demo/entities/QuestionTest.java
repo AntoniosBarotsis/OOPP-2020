6f26b438792238a7df65a3bbac6c7216853f5e92
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2021-04-02 01:02:11 +0200

    Removed redundant method

--- server/src/test/java/nl/tudelft/oopp/demo/entities/QuestionTest.java
+++ server/src/test/java/nl/tudelft/oopp/demo/entities/QuestionTest.java
@@ -49,16 +49,6 @@ class QuestionTest {
     }
 
     @Test
-    @DirtiesContext(methodMode = BEFORE_METHOD)
-    void exportToJson() throws JsonProcessingException {
-        assertThat(q1.exportToJson()).isEqualTo("{\"id\":" + q1.getId()
-                + ",\"text\":\"Question " + "text\",\"answer\":null,\"upvotes\":0,\"score\":0,"
-                + "\"timeCreated\":\"" + q1.getTimeCreated() + "\",\"QuestionStatus\":\"OPEN\","
-                + "\"BeingAnswered\":" + q1.isBeingAnswered() + ",\"author\":{\"id\":"
-                + q1.getAuthor().getId() + ",\"username\":\"Admin\"}}");
-    }
-
-    @Test
     void isAnswered() {
         assertThat(q1.isAnswered()).isEqualTo(false);
         q1.setAnswer("answer");
