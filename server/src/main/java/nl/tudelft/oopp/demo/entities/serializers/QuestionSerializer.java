682cd82f78a2f1f07174b89d86347d453a875d99
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2021-04-02 01:02:51 +0200

    Added try catch block for Null Pointers
    Further investigate why these exceptions are thrown

--- server/src/main/java/nl/tudelft/oopp/demo/entities/serializers/QuestionSerializer.java
+++ server/src/main/java/nl/tudelft/oopp/demo/entities/serializers/QuestionSerializer.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import java.io.IOException;
+import javax.validation.constraints.Null;
 import nl.tudelft.oopp.demo.entities.Question;
 import nl.tudelft.oopp.demo.repositories.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,9 +37,13 @@ public class QuestionSerializer extends StdSerializer<Question> {
     @Override
     public void serialize(Question value, JsonGenerator gen, SerializerProvider provider)
         throws IOException {
-        value = questionRepository.getOne(value.getId());
-        value.updateScore();
-        questionRepository.save(value);
+        try {
+            value = questionRepository.getOne(value.getId());
+            value.updateScore();
+            questionRepository.save(value);
+        } catch (NullPointerException ignored) {
+
+        }
 
         gen.writeStartObject();
         gen.writeNumberField("id", value.getId());
