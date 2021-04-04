e6301311057b77a3897c34aefe19864082eb89ef
Kiril Vasilev <k.v.vasilev-1@student.tudelft.nl>
2021-04-04 19:13:19 +0200

    Test server communication

--- client/src/test/java/nl/tudelft/oopp/demo/communication/startscreen/StartCommunicationTest.java
+++ client/src/test/java/nl/tudelft/oopp/demo/communication/startscreen/StartCommunicationTest.java
@@ -4,31 +4,17 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.Date;
 
+import nl.tudelft.oopp.demo.data.RoomConfig;
+import nl.tudelft.oopp.demo.data.helper.RoomHelper;
 import org.junit.jupiter.api.Test;
 
 public class StartCommunicationTest {
 
     @Test
     void createRoomTest() {
-        assertNotNull(StartCommunication.createRoom("TestRoom", "TestUser"));
+        RoomHelper roomHelper = new RoomHelper("title", "username",
+                true, new RoomConfig(5,5,300,300),
+                "startDate", "endDate");
+        assertNotNull(StartCommunication.createRoom(roomHelper));
     }
-
-    /*
-      Does not work since I need an actual room password to join one
-      Should work once that has been implemented
-    @Test
-    void joinRoomTest() {
-        assertNotNull(StartCommunication.joinRoom("1", "TestName"));
-    }
-    */
-
-    /*
-     Does not work since Date's .toString() inserts spaces.
-     Either a new date constructor or reader.
-     */
-    @Test
-    void createScheduledRoomTest() {
-        assertNotNull(StartCommunication.createScheduledRoom("TestRoom", "TestUser", new Date()));
-    }
-
 }
