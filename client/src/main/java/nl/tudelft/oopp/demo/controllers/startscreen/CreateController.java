0d7a1b5983606683cb3d69017e3f293e4c360d0d
Kiril Vasilev <k.v.vasilev-1@student.tudelft.nl>
2021-04-04 19:10:51 +0200

    Modify create room controller

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/startscreen/CreateController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/startscreen/CreateController.java
@@ -3,7 +3,14 @@ package nl.tudelft.oopp.demo.controllers.startscreen;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Date;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 
 import javafx.fxml.FXMLLoader;
@@ -15,10 +22,13 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
 import javafx.stage.Stage;
+import nl.tudelft.oopp.demo.communication.mainmenu.SettingsCommunication;
 import nl.tudelft.oopp.demo.communication.startscreen.StartCommunication;
 import nl.tudelft.oopp.demo.controllers.mainmenu.MainModController;
 import nl.tudelft.oopp.demo.data.Room;
+import nl.tudelft.oopp.demo.data.RoomConfig;
 import nl.tudelft.oopp.demo.data.User;
+import nl.tudelft.oopp.demo.data.helper.RoomHelper;
 
 public class CreateController {
 
@@ -29,9 +39,21 @@ public class CreateController {
     @FXML
     private CheckBox inputSchedule;
     @FXML
+    private CheckBox inputRepeating;
+    @FXML
     private DatePicker inputDate;
     @FXML
     private TextField inputTime;
+    @FXML
+    private TextField inputTimeEnd;
+    @FXML
+    private TextField tbStudentrefresh;
+    @FXML
+    private TextField tbModRefresh;
+    @FXML
+    private TextField tbQuestionCd;
+    @FXML
+    private TextField tbPaceCd;
 
     /**
      * Checks what type of room is being created and calls correct method.
@@ -43,39 +65,115 @@ public class CreateController {
         String roomName = inputRoomName.getText();
         Room room = null;
         if (roomName.equals("")) {
-            //TODO: Replace this with a error popup
-            roomName = "Room";
+            showAlert("Insert a room name, please.", Alert.AlertType.ERROR);
+            return;
         }
         String username = inputUsername.getText();
         if (username.equals("")) {
-            //TODO: Replace this with a error popup
-            username = "Admin";
+            showAlert("Insert your username, please.", Alert.AlertType.ERROR);
+            return;
         }
-        if (!inputSchedule.isSelected()) {
-            room = StartCommunication.createRoom(roomName, username);
-        } else {
 
-            String date = inputDate.getValue().toString();
-            String time = inputTime.getText();
+        // Fetch room configurations.
+        RoomConfig roomConfig = validatePaceSetting();
+        if (roomConfig == null) {
+            return;
+        }
 
-            try {
-                date = date + " " + time + ":00";
-                room = StartCommunication.createScheduledRoom(roomName, username,
-                        new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse(date));
-            } catch (ParseException e) {
-                e.printStackTrace();
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setHeaderText(null);
-                alert.setContentText("Error parsing date and time.");
-                alert.showAndWait();
+        try {
+            //Fetch date.
+            String startDate = inputDate.getValue() == null ? "" : inputDate.getValue().toString();
+            String startTime = inputTime.getText();
+            String endDate = inputDate.getValue() == null ? "" : inputDate.getValue().toString();
+            String endTime = inputTimeEnd.getText();
+
+            DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
+            String start = LocalDateTime.now().format(pattern);
+            String end = LocalDateTime.now().format(pattern);
+
+            if (inputSchedule.isSelected()) {
+                start = startDate + " " + startTime + ":00";
+                end = endDate + " " + endTime + ":00";
+
             }
+
+            Date checkStart = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse(start);
+            Date checkEnd = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse(end);
+
+            room = StartCommunication.createRoom(new RoomHelper(roomName, username,
+                    inputRepeating.isSelected(), roomConfig, start, end));
+        } catch (ParseException e) {
+            e.printStackTrace();
+            showAlert("Error parsing date and time.", Alert.AlertType.ERROR);
+            return;
         }
 
-        // Getting all the necessary stuff to join a room :D
-        Long roomId = room.getId();
-        String password = StartCommunication.getPrivateCode(roomId);
+        // Check if a room was fetched from server.
+        if (room == null) {
+            showAlert("Error creating a room.", Alert.AlertType.ERROR);
+            return;
+        }
+
+        // Join a room.
+        String password = StartCommunication.getPrivateCode(room.getId());
         User user = StartCommunication.joinRoom(password, username);
         startMainModMenu(room, user);
+    }
+
+    /**
+     * Shows a warning alert on screen.
+     * @param text text to display on alert
+     * @param alertType the type of alert to display
+     */
+    protected void showAlert(String text, Alert.AlertType alertType) {
+        Alert alert = new Alert(alertType);
+        alert.setHeaderText(null);
+        alert.setContentText(text);
+        alert.showAndWait();
+    }
+
+    protected RoomConfig validatePaceSetting() {
+        try {
+            // Validate student refresh rate.
+            int studentRefresh = Integer.parseInt(tbStudentrefresh.getText());
+            if (studentRefresh < 1) {
+                showAlert("Refresh rate for students must be at least 1 second.",
+                        Alert.AlertType.WARNING);
+                return null;
+            }
+
+            // Validate mod refresh rate.
+            int modRefresh = Integer.parseInt(tbModRefresh.getText());
+            if (modRefresh < 1) {
+                showAlert("Refresh rate for moderators must be at least 1 second.",
+                        Alert.AlertType.WARNING);
+                return null;
+            }
+
+            // Validate question cooldown.
+            int questionCooldown = Integer.parseInt(tbQuestionCd.getText());
+            if (questionCooldown < 2 || questionCooldown > 600) {
+                showAlert("Cooldown for asking questions must be "
+                        + "between 2 seconds and 600 seconds.", Alert.AlertType.WARNING);
+                return null;
+            }
+
+            // Validate pace cooldown.
+            int paceCooldown = Integer.parseInt(tbPaceCd.getText());
+            if (paceCooldown < 2 || paceCooldown > 600) {
+                showAlert("Cooldown for setting the pace must be "
+                        + "between 2 seconds and 600 seconds.", Alert.AlertType.WARNING);
+                return null;
+            }
+
+            return new RoomConfig(studentRefresh, modRefresh,
+                    questionCooldown, paceCooldown);
+
+
+        } catch (NumberFormatException e) {
+            showAlert("Textboxes must contain only integer data!", Alert.AlertType.WARNING);
+            return null;
+        }
     }
 
     /**
@@ -100,6 +198,9 @@ public class CreateController {
         stage.setScene(new Scene(root));
         stage.setTitle("Main menu");
         stage.setResizable(false);
+        stage.setOnCloseRequest(e -> {
+            Platform.exit();
+        });
         stage.show();
 
         // Close the previous window
@@ -115,7 +216,7 @@ public class CreateController {
     public void buttonBack() throws Exception {
         Stage stage = (Stage) inputSchedule.getScene().getWindow();
         Parent root = FXMLLoader.load(getClass().getResource("/startView/startScene.fxml"));
-        Scene scene = new Scene(root, 960, 540);
+        Scene scene = new Scene(root, 960, 574);
         stage.setScene(scene);
         stage.show();
     }
