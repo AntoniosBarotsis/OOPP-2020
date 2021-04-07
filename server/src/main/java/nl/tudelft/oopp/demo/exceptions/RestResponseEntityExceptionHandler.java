b0821d8a12a6b0b3ef8b736be2c1dc9c7e89eda9
AntoniosBarotsis <a.barotsis@student.tudelft.nl>
2021-04-07 15:05:14 +0200

    Updated and improved exception handler

--- server/src/main/java/nl/tudelft/oopp/demo/exceptions/RestResponseEntityExceptionHandler.java
+++ server/src/main/java/nl/tudelft/oopp/demo/exceptions/RestResponseEntityExceptionHandler.java
@@ -10,14 +10,38 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
-    @ExceptionHandler(value = {InvalidIdException.class, InvalidPasswordException.class,
-        UnauthorizedException.class})
+    @ExceptionHandler(value = {
+        InvalidIdException.class,
+        InvalidPasswordException.class,
+        InvalidPollStatusException.class,
+        LectureIsOverException.class,
+        UnauthorizedException.class
+    })
     protected ResponseEntity<Object> handleConflict(
         RuntimeException ex, WebRequest request) {
+        HttpStatus status = null;
+
+        String name = ex.getClass()
+            .getName()
+            .replace("nl.tudelft.oopp.demo.exceptions.", "");
+
+        switch (name) {
+            case "InvalidIdException":
+            case "InvalidPollStatusException":
+                status = HttpStatus.NOT_FOUND;
+                break;
+            case "InvalidPasswordException":
+            case "UnauthorizedException":
+                status = HttpStatus.UNAUTHORIZED;
+                break;
+            case "LectureIsOverException":
+                status = HttpStatus.EXPECTATION_FAILED;
+            default:
+                status = HttpStatus.BAD_REQUEST;
+        }
 
         return handleExceptionInternal(ex, "[" + ex.getClass().getName() + "]: "
-                + ex.getLocalizedMessage(), new HttpHeaders(), 
-                HttpStatus.EXPECTATION_FAILED, request);
+                + ex.getLocalizedMessage(), new HttpHeaders(), status, request);
     }
 
 }
