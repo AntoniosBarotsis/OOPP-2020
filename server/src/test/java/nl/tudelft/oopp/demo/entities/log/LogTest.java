506d2469b460c2a02b6e4a9a351eec0940f639c5
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2021-04-09 01:26:32 +0200

    Finished tests

--- server/src/test/java/nl/tudelft/oopp/demo/entities/log/LogTest.java
+++ server/src/test/java/nl/tudelft/oopp/demo/entities/log/LogTest.java
@@ -13,6 +13,7 @@ import nl.tudelft.oopp.demo.entities.RoomConfig;
 import nl.tudelft.oopp.demo.entities.users.ElevatedUser;
 import nl.tudelft.oopp.demo.entities.users.Student;
 import nl.tudelft.oopp.demo.entities.users.User;
+import nl.tudelft.oopp.demo.repositories.LogEntryRepository;
 import nl.tudelft.oopp.demo.repositories.QuestionRepository;
 import nl.tudelft.oopp.demo.repositories.RoomConfigRepository;
 import nl.tudelft.oopp.demo.repositories.RoomRepository;
@@ -39,6 +40,7 @@ class LogTest {
     private User u3;
 
     private Room r1;
+    private Room r2;
 
     private Question question;
 
@@ -52,6 +54,8 @@ class LogTest {
     private QuestionRepository questionRepository;
     @Autowired
     private RoomConfigRepository roomConfigRepository;
+    @Autowired
+    private LogEntryRepository logEntryRepository;
 
     @BeforeEach
     void setUp() {
@@ -67,15 +71,20 @@ class LogTest {
         roomConfigRepository.save(roomConfig);
 
         r1 = new Room("Room Title", false, u1, roomConfig);
-        roomRepository.save(r1);
+        r2 = new Room("Room Title 2", false, u1, roomConfig);
+        roomRepository.saveAll(List.of(r1, r2));
 
         question = new Question("Question Text", u3);
+        questionRepository.save(question);
+
         b1 = new LogBan(r1, u1, "ip", date);
-
         j1 = new LogJoin(u1, r1, date);
-
         q1 = new LogQuestion(r1, (Student) u3, question);
+        b2 = new LogBan(r2, u2, "ip2");
+        j2 = new LogJoin(u2, r2);
+        q2 = new LogQuestion(r2, (Student) u3, question);
 
+        logEntryRepository.saveAll(List.of(b1, j1, q1, b2, j2, q2));
     }
 
     @Test
@@ -143,29 +152,93 @@ class LogTest {
 
     @Test
     void setRoom() {
-    }
+        assertThat(b1.getRoom()).isEqualTo(r1);
+        assertThat(j1.getRoom()).isEqualTo(r1);
+        assertThat(q1.getRoom()).isEqualTo(r1);
 
-    @Test
-    void setAction() {
+        b1.setRoom(r2);
+        j1.setRoom(r2);
+        q1.setRoom(r2);
+
+        assertThat(b1.getRoom()).isEqualTo(r2);
+        assertThat(j1.getRoom()).isEqualTo(r2);
+        assertThat(q1.getRoom()).isEqualTo(r2);
     }
 
     @Test
     void setDate() {
+        Date date = new Date();
+
+        b1.setDate(date);
+        j1.setDate(date);
+        q1.setDate(date);
+
+        assertThat(b1.getDate()).isEqualTo(date);
+        assertThat(j1.getDate()).isEqualTo(date);
+        assertThat(q1.getDate()).isEqualTo(date);
     }
 
     @Test
     void testEquals() {
-    }
+        assertThat(b1).isNotEqualTo(b2);
+        assertThat(j1).isNotEqualTo(j2);
+        assertThat(q1).isNotEqualTo(q2);
 
-    @Test
-    void canEqual() {
+        b2.setRoom(r1);
+        j2.setRoom(r1);
+        q2.setRoom(r1);
+
+        assertThat(b1).isNotEqualTo(b2);
+        assertThat(j1).isNotEqualTo(j2);
+        assertThat(q1).isNotEqualTo(q2);
+
+        b2.setUser(b1.getUser());
+        j2.setUser(j1.getUser());
+        q2.setUser(q1.getUser());
+
+        assertThat(b1).isNotEqualTo(b2);
+        assertThat(j1).isNotEqualTo(j2);
+        assertThat(q1).isNotEqualTo(q2);
+
+        b2.setDate(b1.getDate());
+        j2.setDate(j1.getDate());
+        q2.setDate(q1.getDate());
+
+        assertThat(b1).isNotEqualTo(b2);
+        assertThat(j1).isNotEqualTo(j2);
+        assertThat(q1).isNotEqualTo(q2);
+
+        b2.setId(b1.getId());
+        j2.setId(j1.getId());
+        q2.setId(q1.getId());
+
+        assertThat(b1).isEqualTo(b2);
+        assertThat(j1).isEqualTo(j2);
+        assertThat(q1).isEqualTo(q2);
     }
 
     @Test
     void testHashCode() {
+        assertThat(b1.hashCode()).isNotEqualTo(b2.hashCode());
+        assertThat(j1.hashCode()).isNotEqualTo(j2.hashCode());
+        assertThat(q1.hashCode()).isNotEqualTo(q2.hashCode());
+
+        assertThat(b1.hashCode()).isEqualTo(b1.hashCode());
+        assertThat(b2.hashCode()).isEqualTo(b2.hashCode());
+        assertThat(j1.hashCode()).isEqualTo(j1.hashCode());
+        assertThat(j2.hashCode()).isEqualTo(j2.hashCode());
+        assertThat(q1.hashCode()).isEqualTo(q1.hashCode());
+        assertThat(q2.hashCode()).isEqualTo(q2.hashCode());
     }
 
     @Test
     void testToString() {
+        // its 1 30 in the morning i am not writing this
+        assertThat(b1.toString()).isNotBlank();
+        assertThat(b2.toString()).isNotBlank();
+        assertThat(j1.toString()).isNotBlank();
+        assertThat(j2.toString()).isNotBlank();
+        assertThat(q1.toString()).isNotBlank();
+        assertThat(q2.toString()).isNotBlank();
     }
 }
