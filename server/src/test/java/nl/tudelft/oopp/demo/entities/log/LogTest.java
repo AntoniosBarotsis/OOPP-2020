f9184c7f3bf81e29416f0860f3d49013142bbc6c
Roy Katz <R.Katz-1@student.tudelft.nl>
2021-04-09 00:11:03 +0200

    Edited beforeEach

--- server/src/test/java/nl/tudelft/oopp/demo/entities/log/LogTest.java
+++ server/src/test/java/nl/tudelft/oopp/demo/entities/log/LogTest.java
@@ -2,7 +2,10 @@ package nl.tudelft.oopp.demo.entities.log;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Date;
 import java.util.List;
+
+import nl.tudelft.oopp.demo.entities.Question;
 import nl.tudelft.oopp.demo.entities.Room;
 import nl.tudelft.oopp.demo.entities.users.ElevatedUser;
 import nl.tudelft.oopp.demo.entities.users.Student;
@@ -32,6 +35,8 @@ class LogTest {
 
     private Room r1;
 
+    private Question question;
+
     @Autowired
     private RoomRepository roomRepository;
     @Autowired
@@ -42,10 +47,21 @@ class LogTest {
         u1 = new ElevatedUser("Admin", "ip", true);
         u2 = new ElevatedUser("Mod", "ip");
         u3 = new Student("Student", "ip");
+
         userRepository.saveAll(List.of(u1, u2, u3));
+
+        Date date = new Date();
 
         r1 = new Room("Room Title", false, u1);
         roomRepository.save(r1);
+
+        question = new Question("Question Text", u3);
+        b1 = new LogBan(r1, u1, "ip", date);
+
+        j1 = new LogJoin(u1, r1, date);
+
+        q1 = new LogQuestion(r1, (Student) u3, question);
+
     }
 
     @Test
